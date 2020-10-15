0f25e30055e5c4b2b236f48f5d76183b0bf06328
Tara Kelly <kellyt2@students.westerntc.edu>
2020-10-15 14:50:53 -0500

    Add some simple model classes

---
+++ PizzaBot/PizzaBot/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaBot.Models
+{
+    public class Order
+    {
+        public List<OrderItem<Object>> OrderItems { get; set; }  // TODO: not sure what to put for T
+        public Customer Customer { get; set; }
+    }
+}
---
+++ PizzaBot/PizzaBot/Models/OrderItem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaBot.Models
+{
+    public class OrderItem<T> 
+    {
+        public T Type { get; set; }
+        public decimal Price { get; set; }
+        public int Qty { get; set; }
+        public class Pizza
+        {
+            public List<Topping> MyProperty { get; set; }
+            public enum Size
+            {
+                Small,
+                Medium,
+                Large
+            }
+
+            public enum Crust
+            {
+                Thin,
+                HandTossed,
+                DeepDish
+            }
+
+            public enum Topping
+            {
+                Sausage,
+                Pepperoni,
+                Mushroom,
+                Onion,
+                GreenPepper,
+                ExtraCheese
+            }
+        }
+
+        public enum SideItem
+        {
+            Breadsticks,
+            Wings
+        }
+
+        public enum Drink
+        {
+            Pepsi,
+            DietPepsi,
+            Coke,
+            DietCoke,
+            RootBeer, 
+            Sprite
+        }
+    }
+}
